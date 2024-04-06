32fb07e5647ce378782b4c6de8b273b96c1100ef
ethan-waldo <ethanwaldo9@gmail.com>
2024-04-06 04:13:35 -0400

    remove some unused files

--- app/projects/page.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@/utils/supabase/server'
-
-export default async function Notes() {
-  const supabase = createClient();
-  const { data: projects } = await supabase.from("projects").select();
-
-  return <pre>{JSON.stringify(projects, null, 2)}</pre>
-}
