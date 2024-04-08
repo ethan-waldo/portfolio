d4d6219885e36a3d9c7bf63bbb6ba8395c72822c
ethan-waldo <ethanwaldo9@gmail.com>
2024-04-08 15:27:54 -0400

    styling edits

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import { PortfolioCarousel } from "@/components/portfolio-carousel";
 
 export default async function Home() {        
   return (
-    <div className="absolute inset-0 h-full w-full bg-background bg-[radial-gradient(gray,transparent_1px)] [background-size:32px_32px]">
+    <div className="absolute inset-0 h-full w-full bg-background bg-[radial-gradient(gray,transparent_1px)] [background-size:64px_64px]">
     <main className="relative flex min-h-screen flex-col items-center justify-center gap-4 p-12">
       <div className="absolute top-0 right-0 m-2">
         <ModeToggle/>
       </div>
-      <h1 className="font-black scroll-m-20 text-4xl tracking-tight lg:text-5xl">
+      <h1 className="font-black text-4xl tracking-tight">
         Ethan Waldo
       </h1>
       <PortfolioCarousel/>
